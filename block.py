8ecb5e1c68d58f7ebf7172122551f02b78a6e337
Tony Lau <ual_ynot@yahoo.com>
2020-05-17 15:15:17 -0700

    Fixed some pylint complaints

--- block.py
+++ block.py
@@ -8,7 +8,7 @@ class Block:
         self.nonce = nonce
         # root hash of merkle tree
         self.root_hash = root_hash
-    
+
     def to_string(self):
         self_dict = {
             "prev_hash": self.prev_hash,
@@ -21,12 +21,12 @@ class GenesisBlock:
     pass
 
 class LogicalBlock:
-    def __init__(self, block, tree=None)
+    def __init__(self, block, tree=None):
         # actual block
         self.block = block
         # merkle tree of transactions attached to block
         self.tree = tree
-    
+
     def get_transaction(self, i):
         # look through merkle tree for transaction
         pass
--- transaction.py
+++ transaction.py
@@ -6,11 +6,12 @@ class Transaction:
         self.src_block = src_block
         self.src_transact = src_transact
         self.dst_addr = dst_addr
-    
+
     def to_string(self, blockchain, prv_key):
         self_dict = {
             "dst_addr": self.dst_addr,
             # can actually be whatever if signature is that of issuer
-            "hash": blockchain[src_block].get_transaction(src_transact),
+            "hash": blockchain[self.src_block].get_transaction(self.src_transact),
             "signature": prv_key.sign("Something")
         }
+        return json.dumps(self_dict)
